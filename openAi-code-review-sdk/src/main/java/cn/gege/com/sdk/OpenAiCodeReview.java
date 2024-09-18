e9d9de95b599806a90c5c8e34a0ddc5a98e2bf0f
2501843057 <2501843057@qq.com>
2024-09-18 22:48:09 +0800

    feat: init project

--- openAi-code-review-sdk/src/main/java/cn/gege/com/sdk/OpenAiCodeReview.java
+++ openAi-code-review-sdk/src/main/java/cn/gege/com/sdk/OpenAiCodeReview.java
@@ -2,7 +2,7 @@ package cn.gege.com.sdk;
 
 public class OpenAiCodeReview {
     public static void main(String[] args) {
-        System.out.println("测试");
+        System.out.println("测试gege");
     }
 
 }
